d84dcfcd16cd826c64ca3188626a6de828a4a955
Julia <berrylily.0709@gmail.com>
2017-12-07 13:03:10 -0800

    Generate html

--- djangogirls/development/blog/views.py
+++ djangogirls/development/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # parse request
+    # access databese
+    # get data
+    # sending it back...
+
+    # get all my posts from the database.
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def about(request):
+    return render(request, 'blog/about.html')
